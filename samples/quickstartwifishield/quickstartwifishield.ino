b2d4d7effc7c3e6ed750bc16548bfc5c78ef4d40
Amit M Mangalvedkar <amitmangalvedkar@gmail.com>
2015-06-26 11:51:15 +0530

    Added Wifi shield code for quickstart

---
+++ samples/quickstartwifishield/quickstartwifishield.ino
@@ -0,0 +1,124 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <WiFi.h>
+#include <IPStack.h>
+#include <Countdown.h>
+#include <MQTTClient.h>
+#define MS_PROXY "quickstart.messaging.internetofthings.ibmcloud.com"
+#define MQTT_PORT 1883
+#define MQTT_MAX_PACKET_SIZE 100
+
+// Update these with values suitable for your network.
+byte mac[] = { 0x11, 0xBB, 0xCC, 0xDD, 0xEF, 0x03 };
+//The convention to be followed is d:quickstart:iotsample-arduino:<MAC Address>
+
+#define MQTT_CLIENTID "d:quickstart:iotsample-arduino:11bbccddef03"
+#define MQTT_TOPIC "iot-2/evt/status/fmt/json"
+
+//For Arduino Yun, instantiate a YunClient and use the instance to declare
+//an IPStack ipstack(c) instead of EthernetStack with c being the YunClient
+// e.g. YunClient c;
+// IPStack ipstack(c);
+//EthernetClient c; // replace by a YunClient if running on a Yun
+WiFiClient c;
+IPStack ipstack(c);
+
+MQTT::Client<IPStack, Countdown, 100, 1> client = MQTT::Client<IPStack, Countdown, 100, 1>(ipstack);
+
+String deviceEvent;
+
+char ssid[] = "Your Wifi Network";     //  your network SSID (name)
+char pass[] = "Your Wifi Password";  // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+  String fv = WiFi.firmwareVersion();
+  if ( fv != "1.1.0" )
+    Serial.println("Please upgrade the firmware");
+
+  // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 5 seconds for connection:
+    delay(5000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network\n");  
+}
+
+void loop() {
+  int rc = -1;
+  if (!client.isConnected()) {
+    Serial.println("Connecting to IoT Foundation for publishing Temperature");
+    while (rc != 0) {
+      rc = ipstack.connect(MS_PROXY, MQTT_PORT);
+    }
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = MQTT_CLIENTID;
+    rc = -1;
+    while ((rc = client.connect(data)) != 0)
+    ;
+    Serial.println("Connected successfully\n");
+    Serial.println("Temperature(in C)\tDevice Event (JSON)");
+    Serial.println("____________________________________________________________________________");
+  }
+  Serial.println("\n");
+  MQTT::Message message;
+  message.qos = MQTT::QOS0;
+  message.retained = false;
+  deviceEvent = String("{\"d\":{\"myName\":\"Arduino Uno\",\"temperature\":");
+  char buffer[60];
+  // convert double to string 
+  dtostrf(getTemp(),1,2, buffer);
+  deviceEvent += buffer;
+  deviceEvent += "}}";
+  Serial.print("\t");
+  Serial.print(buffer);
+  Serial.print("\t\t");
+  deviceEvent.toCharArray(buffer, 60);
+  Serial.println(buffer);
+  message.payload = buffer;
+  message.payloadlen = strlen(buffer);
+  rc = client.publish(MQTT_TOPIC, message);
+
+  if (rc != 0) {
+    Serial.print("return code from publish was ");
+    Serial.println(rc);
+  }
+  client.yield(1000);
+}
+
+/*
+This function is reproduced as is from Arduino site => http://playground.arduino.cc/Main/InternalTemperatureSensor
+*/
+double getTemp(void) {
+  unsigned int wADC;
+  double t;
+  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+  ADCSRA |= _BV(ADEN); // enable the ADC
+  delay(20); // wait for voltages to become stable.
+  ADCSRA |= _BV(ADSC); // Start the ADC
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
+  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
+  wADC = ADCW;
+  // The offset of 324.31 could be wrong. It is just an indication.
+  t = (wADC - 324.31 ) / 1.22;
+  // The returned temperature is in degrees Celcius.
+  return (t);
+}
